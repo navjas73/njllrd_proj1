d161564c70f5ace4dd744ce32fd9fb84fbd0b5e6
Naveen Jasty <navjas73@gmail.com>
2015-09-20 17:08:15 -0400

    Added test of stack_ascending to controller node

--- scripts/controller.py
+++ scripts/controller.py
@@ -2,19 +2,21 @@
 import rospy
 from njllrd_proj1.srv import *
 from njllrd_proj1.msg import *
+import time
+from std_msgs.msg import String
 
 # When topic "command" is published to, reads block orientation mode and
 #   starts relevant process
 def mode_selection(data):
-    if data.data == "scatter"
+    if data.data == "scatter":
         scatter()
-    else if data.data = "stack_ascending"
+    elif data.data == "stack_ascending":
         stack_ascending()
-    else if data.data = "stack_descending"
+    elif data.data == "stack_descending":
         stack_descending()
-    else if data.data = "odd_even"
+    elif data.data == "odd_even":
         odd_even()
-    else 
+    else: 
         print("Invalid mode")
 
 
@@ -22,30 +24,39 @@ def mode_selection(data):
 def request_movement(action, target):
     try:
         request = rospy.ServiceProxy('move_robot', move_robot)
-        if request == False
+        if request == False:
             print "Invalid movement"
-        else
+        else:
             print "Successfully moved"
     except rospy.ServiceException, e:
         print "Service call failed: %s" %e
     
 def scatter():
     # Code for moving blocks with scattered orientation
+    return
 
 def stack_ascending():
     # Code for moving blocks with stacked ascending orientation
+    request_movement(close_gripper, 1)
+    request_movement(move_over_block, 0) # virtual block (table)
+    request_movement(open_gripper, 1)
 
 def stack_descending():
     # Code for moving blocks with stacked descending orientation
+    return
 
 def odd_even():
+    return
 
 def read_state():
+    return
 
 def controller():
     rospy.init_node('controller')
     rospy.Subscriber('command', String, mode_selection)
     rospy.Subscriber('state', state, read_state)
+    rospy.wait_for_service('move_robot')
+    time.sleep(1)
     rospy.spin()
 
 if __name__ == "__main__":
